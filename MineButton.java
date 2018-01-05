fd6ff35ff57f1c25101c65b016be1291439b4a83
Trollicorn <muddin3@stuy.edu>
2018-01-05 14:35:49 -0500

    created MineButton class that will be used in the gui instead of JButton class only contains fields and constructors

---
+++ MineButton.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class MineButton extends JButton{
+
+	private boolean flagged;
+	private boolean covered;
+	private int minesAround;
+
+	public MineButton(){
+		super();
+	}
+
+	public MineButton(Icon label){
+		super(label);
+	}
+
+	
+
+
+
+
+}
