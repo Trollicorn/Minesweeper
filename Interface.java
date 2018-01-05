4e736d23606de1975892b0eda67e9ec2f120b170
ecrystale <elee19@stuy.edu>
2018-01-05 09:32:20 -0500

    trying commit

---
+++ Interface.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Interface extends JFrame implements ActionListener{
+    private Container pane;
+    public void actionPerformed(ActionEvent e){
+
+    }
+    public Interface(){
+	this.setTitle("MineSweeper");
+    }
+}
