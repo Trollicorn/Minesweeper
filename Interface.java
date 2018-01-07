f54453e2efb529f1716815d7bb568a2c33e74d05
Emily <elee19@stuy.edu>
2018-01-07 14:21:21 -0500

    basic interface; needs improvement

--- Interface.java
+++ Interface.java
@@ -3,10 +3,26 @@ import java.awt.*;
 import java.awt.event.*;
 public class Interface extends JFrame implements ActionListener{
     private Container pane;
+
     public void actionPerformed(ActionEvent e){
 
     }
+
     public Interface(){
 	this.setTitle("MineSweeper");
+	this.setSize(500,500);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane=this.getContentPane();
+	pane.setLayout(new FlowLayout());
+	for (int i=0; i<100; i++){
+	    pane.add(new JButton());
+	}
+    }
+
+    public static void main(String[] args) {
+        Interface g = new Interface();
+        g.setVisible(true);
     }
 }
