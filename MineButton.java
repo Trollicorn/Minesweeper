fdb7fa305b87354664a2265cc32166d96e0902b4
Trollicorn <muddin3@stuy.edu>
2018-01-08 14:24:43 -0500

    changed MineButton constructors and added getters and setters

--- MineButton.java
+++ MineButton.java
@@ -2,19 +2,57 @@ import javax.swing.*;
 
 public class MineButton extends JButton{
 
+	private char[] board;
+	private int row;
+	private int col;
 	private boolean flagged;
 	private boolean covered;
 	private int minesAround;
 
-	public MineButton(){
+	//Constructors 
+
+	public MineButton(char[] board, int row, int col){
 		super();
+		this.board = board;
+		this.row = row;
+		this.col = col;
+		flagged = false;
+		covered = true;
 	}
 
-	public MineButton(Icon label){
-		super(label);
+	//getters
+
+	public boolean isFlagged(){
+		return flagged;
 	}
 
-	
+	public boolean isCovered(){
+		return covered;
+	}
+
+	public int getRow(){
+		return row;
+	}
+
+	public int getCol(){
+		return col;
+	}
+
+	//setters 
+
+	public void uncover(){
+		covered = false;
+	}
+
+	public void flag(){
+		flagged = !flagged;
+	}
+
+
+
+//	public int minesAround(){
+		
+//	}
 
 
 
