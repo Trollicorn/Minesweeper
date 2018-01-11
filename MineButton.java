e9ce3c578e79ae173112d2d259502ec6f03d30b9
Emily <elee19@stuy.edu>
2018-01-10 23:07:21 -0500

    adding a game over option

--- MineButton.java
+++ MineButton.java
@@ -2,96 +2,108 @@ import javax.swing.*;
 
 public class MineButton extends JButton{
 
-	private char[][] board;
-	private int row;
-	private int col;
-	private boolean flagged;
-	private boolean covered;
-	private int minesAround;
+    private char[][] board;
+    private int row;
+    private int col;
+    private boolean flagged;
+    private boolean covered;
+    private int minesAround;
+    private boolean endgame=false;
+    private boolean minehit=false;
 
-	//Constructors 
+    //Constructors 
 
-	public MineButton(char[][] board, int row, int col){
-		super();
-		this.board = board;
-		this.row = row;
-		this.col = col;
-		flagged = false;
-		covered = true;
-		minesAround = countMinesAround();
+    public MineButton(char[][] board, int row, int col){
+	super();
+	this.board = board;
+	this.row = row;
+	this.col = col;
+	flagged = false;
+	covered = true;
+	minesAround = countMinesAround();
+    }
+
+    //getters
+
+    public boolean isFlagged(){
+	return flagged;
+    }
+
+    public boolean isCovered(){
+	return covered;
+    }
+
+    public int getRow(){
+	return row;
+    }
+
+    public int getCol(){
+	return col;
+    }
+
+    //setters 
+
+    public void uncover(){
+	covered = false;
+	if (board[getRow()][getCol()] == 'm'){
+	    setLabel(""+board[getRow()][getCol()]);
+	    endgame=true;
+	    minehit=true;
+	}else{
+	    setLabel(""+countMinesAround());
 	}
+    }
 
-	//getters
+    public boolean getminehit(){
+	return minehit;
+    }
 
-	public boolean isFlagged(){
-		return flagged;
+    public boolean getendgame(){
+	return endgame;
+    }
+
+    public void flag(){
+	flagged = !flagged;
+	if (isFlagged()){
+	    setLabel("f");
+	}else{
+	    setLabel("-");
 	}
+    }
 
-	public boolean isCovered(){
-		return covered;
+    public int countMinesAround(){
+	int count = 0; 
+	boolean notRowZero = getRow() != 0; 
+	boolean notColZero = getCol() != 0; 
+	boolean notRowLast = getRow() != board.length - 1;
+	boolean notColLast = getCol() != board[0].length - 1;
+
+	if (notRowZero && notColZero && board[getRow()-1][getCol()-1] == 'm'){  // top left
+	    count += 1;
 	}
-
-	public int getRow(){
-		return row;
+	if (notRowZero && board[getRow()-1][getCol()] == 'm'){                  // top mid 
+	    count += 1;
 	}
-
-	public int getCol(){
-		return col;
+	if (notRowZero && notColLast && board[getRow()-1][getCol()+1] == 'm'){                // top right 
+	    count += 1;
 	}
-
-	//setters 
-
-	public void uncover(){
-		covered = false;
-		if (board[getRow()][getCol()] == 'm'){
-			setLabel(""+board[getRow()][getCol()]);
-		}else{
-			setLabel(""+countMinesAround());
-		}
+	if (notColLast && board[getRow()][getCol()+1] == 'm'){                  // mid right 
+	    count += 1;
 	}
-
-	public void flag(){
-		flagged = !flagged;
-		if (isFlagged()){
-			setLabel("f");
-		}else{
-			setLabel("-");
-		}
+	if (notRowLast && notColLast && board[getRow()+1][getCol()+1] == 'm'){  // bot right 
+	    count += 1;
 	}
-
-	public int countMinesAround(){
-		int count = 0; 
-		boolean notRowZero = getRow() != 0; 
-		boolean notColZero = getCol() != 0; 
-		boolean notRowLast = getRow() != board.length - 1;
-		boolean notColLast = getCol() != board[0].length - 1;
-
-		if (notRowZero && notColZero && board[getRow()-1][getCol()-1] == 'm'){  // top left
-			count += 1;
-		}
-		if (notRowZero && board[getRow()-1][getCol()] == 'm'){                  // top mid 
-			count += 1;
-		}
-		if (notRowZero && notColLast && board[getRow()-1][getCol()+1] == 'm'){                // top right 
-			count += 1;
-		}
-		if (notColLast && board[getRow()][getCol()+1] == 'm'){                  // mid right 
-			count += 1;
-		}
-		if (notRowLast && notColLast && board[getRow()+1][getCol()+1] == 'm'){  // bot right 
-			count += 1;
-		}
-		if (notRowLast && board[getRow()+1][getCol()] == 'm'){                  // bot mid 
-			count += 1;
-		}
-		if (notRowLast && notColZero && board[getRow()+1][getCol()-1] == 'm'){  // bot left 
-			count += 1;
-		}
-		if (notColZero && board[getRow()][getCol()-1] == 'm'){                  // mid left 
-			count += 1;
-		}
-		return count;
+	if (notRowLast && board[getRow()+1][getCol()] == 'm'){                  // bot mid 
+	    count += 1;
 	}
+	if (notRowLast && notColZero && board[getRow()+1][getCol()-1] == 'm'){  // bot left 
+	    count += 1;
+	}
+	if (notColZero && board[getRow()][getCol()-1] == 'm'){                  // mid left 
+	    count += 1;
+	}
+	return count;
+    }
 
 
 
--- MineListener.java
+++ MineListener.java
@@ -1,24 +1,54 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class MineListener extends MouseAdapter{
 
-	private MineButton myButton;
+    private Container endpane;
+    private MineButton myButton;
+    private JTextField text;
 
-	public MineListener(MineButton button){
-		myButton = button;
-	}
+    public MineListener(MineButton button){
+	myButton = button;
+    }
 
-	public void mouseClicked(MouseEvent click){
-		if (myButton.isCovered()){
-			if (SwingUtilities.isLeftMouseButton(click) && !myButton.isFlagged()){
-				myButton.uncover();
-			}
-			if (SwingUtilities.isRightMouseButton(click)){
-				myButton.flag();
-			}
+    public void mouseClicked(MouseEvent click){
+	if (myButton.isCovered()){
+	    if (SwingUtilities.isLeftMouseButton(click) && !myButton.isFlagged()){
+		myButton.uncover();
+		if (myButton.getendgame()==true){
+		    GameOver();
 		}
+	    }
+	    if (SwingUtilities.isRightMouseButton(click)){
+		myButton.flag();
+	    }
 	}
+    }
 
+    public void GameOver(){
+	if (myButton.getminehit()==true){
+	    EndPage("Game Over. You Lose.");
+	}
+	else{
+	    EndPage("Congratulations! You won!");
+	}
+    }
+
+    public void EndPage(String message){
+	this.setTitle("The Game Has Ended");
+	this.setSize(200,200);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	endpane=this.getContentPane();
+	endpane.setLayout(new FlowLayout());
+
+	text= new JTextField(30);
+	text.setText(message);
+	text.setEditable(false);
+
+	endpane.setVisible(true);
+    }
 
 }
