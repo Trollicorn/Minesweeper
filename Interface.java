330f7486e1e5266a3bf2951fb1ec6feddda32ef6
ecrystale <elee19@stuy.edu>
2018-01-19 14:39:13 -0500

    add menu to master

--- Interface.java
+++ Interface.java
@@ -1,50 +1,83 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Interface extends JFrame{
-	private Container pane;
-	private GridLayout grid=new GridLayout(10,10,1,1);
-	private char[][] board = new char[10][10];
+public class Interface extends JFrame implements ActionListener{
+    private Container pane;
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private GridLayout grid=new GridLayout(10,10,1,1);
+    private char[][] board = new char[10][10];
+    private JButton newgame;
 
 
-	public Interface(){
-		this.setTitle("MineSweeper");
-		this.setSize(500,500);
-		this.setLocation(100,100);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public void actionPerformed(ActionEvent e){
+	String s=e.getActionCommand();
+	if (s.equals("New Game")){
+	    this.dispose();
+	    System.out.println(s);
+	    Interface f= new Interface();
+	    f.setVisible(true);
+	}
+    }
+    
+    public Interface(){
+	reset();
+	menuset();
+    }
+    
+    public void reset(){
+	this.setTitle("MineSweeper");
+	this.setSize(500,500);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 
 
 	//BorderPane pane= new BorderPane();
-		pane=this.getContentPane();
-		pane.setLayout(grid);
-		pane.setBackground(Color.BLUE);
-		makeBoard();
-		for (int i=0; i < board.length * board[0].length; i++){
-			MineButton button = new MineButton(board, i / 10, i % 10);
-			//button.setForeground(Color.WHITE);
-			button.addMouseListener(new MineListener(button));
-			button.setText("-");
-			pane.add(button);
+	pane=this.getContentPane();
+	pane.setLayout(grid);
+	pane.setBackground(Color.BLUE);
+	makeBoard();
+	for (int i=0; i < board.length * board[0].length; i++){
+	    MineButton button = new MineButton(board, i / 10, i % 10);
+	    button.setForeground(Color.WHITE);
+	    button.addMouseListener(new MineListener(button));
+	    button.setText("-");
+	    pane.add(button);
+	}
+
+    }
+
+    public void menuset(){
+	menuBar= new JMenuBar();
+
+	menu=new JMenu("Options");
+	menu.getAccessibleContext().setAccessibleDescription("Select Options");
+
+	newgame= new JButton ("New Game");
+	newgame.addActionListener(this);
+	//newgame.setBounds(11,11,10,1);
+	menu.add(newgame);
+
+	menuBar.add(menu);
+	setJMenuBar(menuBar);
+    }
+    
+    public void makeBoard(){
+	int minesWanted = 15;
+	for (int i = 0; i < board.length; i++){
+	    for (int j = 0; j < board[i].length; j++){
+		if ((int)(Math.random() * 1000) % minesWanted == 0){
+		    board[i][j] = 'm';
+		}else{
+		    board[i][j] = '-';
 		}
-
+	    }
 	}
+    }
 
-	public void makeBoard(){
-		int minesWanted = 15;
-		for (int i = 0; i < board.length; i++){
-			for (int j = 0; j < board[i].length; j++){
-				if ((int)(Math.random() * 1000) % minesWanted == 0){
-					board[i][j] = 'm';
-				}else{
-					board[i][j] = '-';
-				}
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		Interface g = new Interface();
-		g.setVisible(true);
-	}
+    public static void main(String[] args) {
+	Interface g = new Interface();
+	g.setVisible(true);
+    }
 }
